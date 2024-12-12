0f15d4f125ead0950d1f16902aa99cc32f328397
Dominica <dominica.dawson@gmail.com>
2024-12-11 21:02:03 -0600

    WIP Homework 19
    Create a new branch named homework-19.
    
    2. Create a new file Homework19.java in IntelliJ IDEA.
    
    3. Create a test case, deletePlaylist() using @Test annotation.
    
    4. Use the helper/reusable methods we created.
    
    5. Use @Parameters for passing baseUrl from the TestNG config file to the tests.
    
    6. Navigate to "https://qa.koel.app/".
    
    7. Log in with your credentials.
    
    8. Click the playlist you want to delete.
    
    9. We should see a red button "x PLAYLIST" on the top right part of the page, and click on it.
    
    Note: If the red button is not displayed, let's create a new playlist and click it.
    
    10. Verify that the confirmation notification displayed has the text, "Deleted playlist {playlist name}".

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,8 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,7 +8,6 @@ import java.time.Duration;
 public class LoginTests extends BaseTest {
     @Test
     public void loginEmptyEmailPassword()  {
-
         WebElement loginField = driver.findElement(By.cssSelector("[type='email']"));
         loginField.sendKeys("planner@testpro.io");
         WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
